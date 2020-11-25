b4c2e6c0a2813b18bd7c4e8aabc8f515446dc6f1
Satish Kumar <satish.kumar06@nagarro.com>
2020-11-25 14:04:42 +0530

    invoke static method, variables and creating objects
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

--- src/main/java/com/springcore/SpEL/Demo.java
+++ src/main/java/com/springcore/SpEL/Demo.java
@@ -12,6 +12,15 @@ public class Demo {
 	@Value("#{2+5+56+34}")
 	private int y;
 	
+	@Value("#{ T(java.lang.Math).sqrt(144)}")
+	private double z;
+	
+	@Value("#{ T(java.lang.Math).E }")
+	private double e;
+	
+	@Value("#{new java.lang.String('Satish Kumar Gupta')}")
+	private String name;
+	
 	public int getX() {
 		return x;
 	}
@@ -24,10 +33,21 @@ public class Demo {
 	public void setY(int y) {
 		this.y = y;
 	}
-	
+	public double getZ() {
+		return z;
+	}
+	public void setZ(double z) {
+		this.z = z;
+	}
+	public double getE() {
+		return e;
+	}
+	public void setE(double e) {
+		this.e = e;
+	}
 	@Override
 	public String toString() {
-		return "Demo [x=" + x + ", y=" + y + "]";
+		return "Demo [x=" + x + ", y=" + y + ", z=" + z + ", e=" + e + ", name=" + name + "]";
 	}
-	
+			
 }
--- src/main/java/com/springcore/SpEL/Test.java
+++ src/main/java/com/springcore/SpEL/Test.java
@@ -2,8 +2,6 @@ package com.springcore.SpEL;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.expression.Expression;
-import org.springframework.expression.spel.standard.SpelExpressionParser;
 
 public class Test {
 
@@ -15,12 +13,6 @@ public class Test {
 		Demo d1 = context.getBean("demo", Demo.class);
 		
 		System.out.println(d1);
-		
-		System.out.println("______________________");
-		
-		SpelExpressionParser temp = new SpelExpressionParser();
-		Expression expression = temp.parseExpression("22+44");
-		System.out.println(expression.getValue());
 
 	}
 
