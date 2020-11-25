8c3f24c6527fa0a3664effdb327cfa5680dfb027
Satish Kumar <satish.kumar06@nagarro.com>
2020-11-25 14:31:38 +0530

    Removing Complete XML for Spring Configuration (@Configuration | @ComponentScan
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

---
+++ src/main/java/com/springcore/javaconfig/Client.java
@@ -0,0 +1,20 @@
+package com.springcore.javaconfig;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Client {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context =
+				new AnnotationConfigApplicationContext(JavaConfig.class);
+		
+		Student student = context.getBean("student", Student.class);
+		System.out.println(student);
+		student.study();
+		
+
+	}
+
+}
